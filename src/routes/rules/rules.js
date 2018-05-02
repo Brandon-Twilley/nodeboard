d0ebf87c37bce7e2a4f5edfbcfc51ae97c18a369
Brandon <brandon.twilley@okstate.edu>
2018-05-01 19:22:27 -0500

    added global rules to messaging board

--- src/routes/rules/rules.js
+++ src/routes/rules/rules.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
 
 router.get('/rules', function(request, response) {
-	response.render('rules/rules', {title: 'Rules'});
+	var obj;
+	fs.readFile('./src/json_data/rules.json', 'utf8', function(err, data){
+		if (err) throw err;
+		//response.send(data);
+		obj = JSON.parse(data);
+		//response.send(JSON.stringify(obj));
+		response.render('rules/rules', obj);
+	});
 });
 
 module.exports = router;
