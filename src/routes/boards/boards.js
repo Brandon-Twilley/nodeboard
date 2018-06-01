2b6950cba8225422dce5ce84c52695eb2208dac4
Brandon <brandon.twilley@okstate.edu>
2018-06-01 06:06:55 -0500

    added description sidebar to board thread and changed API to retrieve most recently updated board threads

--- src/routes/boards/boards.js
+++ src/routes/boards/boards.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const init = require('../../../initialize');
 const app = express();
+const mysql = require('mysql');
+
 
 
 router.get('/boards/view/', function(request, response, next){
@@ -18,16 +20,19 @@ router.get('/boards/view/', function(request, response, next){
 
 router.get('/boards/view/:tagid', function(request, response, next){
 	var object = {};
-	init.setup_webpage_object(object, request, function(object){
-		object.board = request.params.tagid;
-		console.log('before get_threads: ' + JSON.stringify(object.board));
-		init.get_threads(object, function(object){
-			object.title = "Boards";
-			console.log(JSON.stringify(object));
-			response.render('thread/view', object);
+	init.setup_webpage_object(object, request, function(webpage_object){
+		webpage_object.board = request.params.tagid;
+		console.log('before get_threads: ' + JSON.stringify(webpage_object.board));
+
+		init.get_boards_shortname(webpage_object, function(webpage_object){
+			webpage_object.title = "Board: " + webpage_object.board;
+			response.render('thread/view', webpage_object);
+			console.log(JSON.stringify(webpage_object));
+
 		});
 	});
 });
+
 
 router.get('/boards/add', function(request, response, next){
 	var object = {};
@@ -35,6 +40,7 @@ router.get('/boards/add', function(request, response, next){
 		object.title = "add board";
 		response.render('boards/add', object);
 	});
-})
+});
+
 
 module.exports = router;
